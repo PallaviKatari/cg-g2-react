be5671aa3e76133ccc4140ba71369cdd036e5b0c
PallaviKatari <pallavikatari@gmail.com>
2023-06-09 14:21:22 +0530

    commit_09.06.2023

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -3,48 +3,61 @@ import logo from "../Assets/react.png";
 
 export default function Carousel() {
   return (
-    <>
-      <div
-        id="carouselExampleControls"
-        classNameName="carousel slide"
-        data-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={logo} className="d-block w-100" alt="..." />
+    <div className="container-fluid" style={{marginTop:100}}>
+      <div id="myCarousel" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <div className="carousel-inner" role="listbox">
+          <div className="item active">
+            <img src={logo} alt="HTML" />
+            <h3>HTML</h3>
+
+            <p>Hypertext Markup Language</p>
           </div>
-          <div className="carousel-item">
-            <img src={logo} className="d-block w-100" alt="..." />
+
+          <div className="item">
+            <img src={logo} alt="CSS" />
+            <h3>CSS</h3>
+            <p>Cascading Style Sheet</p>
           </div>
-          <div className="carousel-item">
-            <img src={logo} className="d-block w-100" alt="..." />
+
+          <div className="item">
+            <img src={logo} alt="JS" />
+            <h3>JS</h3>
+
+            <p>Javascript</p>
           </div>
         </div>
+
         <a
-          className="carousel-control-prev"
-          href="#carouselExampleControls"
+          className="left carousel-control"
+          href="#myCarousel"
           role="button"
           data-slide="prev"
         >
           <span
-            className="carousel-control-prev-icon"
+            className="glyphicon glyphicon-chevron-left"
             aria-hidden="true"
           ></span>
           <span className="sr-only">Previous</span>
         </a>
         <a
-          className="carousel-control-next"
-          href="#carouselExampleControls"
+          className="right carousel-control"
+          href="#myCarousel"
           role="button"
           data-slide="next"
         >
           <span
-            className="carousel-control-next-icon"
+            className="glyphicon glyphicon-chevron-right"
             aria-hidden="true"
           ></span>
           <span className="sr-only">Next</span>
         </a>
       </div>
-    </>
+    </div>
   );
 }
